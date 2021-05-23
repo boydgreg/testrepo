07c9824371fb30aafbe90d325581cc93cde4d5b4
boydgreg <boydgreg@hotmail.com>
2021-05-23 11:20:39 -0400

    This works!

--- app.js
+++ app.js
@@ -1,2 +1,2 @@
 let d = new Date();
-document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds()</h1>"
+document.body.innerHTML = "<h1>Time right now is:  " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "</h1>"
